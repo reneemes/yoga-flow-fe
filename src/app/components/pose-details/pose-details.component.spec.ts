b34971aa94408162380e3920e595140bd207da13
Renee Messersmith <morganrmessersmith@gmail.com>
2025-07-03 14:48:03 -0400

    Test: Pose Details component unit testing complete

--- src/app/components/pose-details/pose-details.component.spec.ts
+++ src/app/components/pose-details/pose-details.component.spec.ts
@@ -1,23 +1,99 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { PoseDetailsComponent } from './pose-details.component';
+import { PosesFetchService } from '../../services/poses-fetch/poses-fetch.service';
+import { HttpStatusCode, provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { of, throwError } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 
-// import { PoseDetailsComponent } from './pose-details.component';
+describe('PoseDetailsComponent', () => {
+  let component: PoseDetailsComponent;
+  let fixture: ComponentFixture<PoseDetailsComponent>;
 
-// describe('PoseDetailsComponent', () => {
-//   let component: PoseDetailsComponent;
-//   let fixture: ComponentFixture<PoseDetailsComponent>;
+  const mockPose = {
+    "data": {
+      "id": 1,
+      "type": "pose",
+      "attributes": {
+        "name": "Downward-Facing Dog",
+        "sanskrit_name": "Parivṛtta Adho Mukha Svanasana",
+        "pose_description": "From downward_dog.html the legs are straight with the sits bones tilted up and reaching for the sky...",
+        "translation_name": "parivṛtta = revolved, adho = downward, mukha = facing, śvāna = dog, āsana = posture",
+        "pose_benefits": "Stretches the hamstrings, calves, and spine while building strength in the arms and legs.",
+        "image_url": "URL"
+      }
+    }
+  };
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [PoseDetailsComponent]
-//     })
-//     .compileComponents();
+  const mockedId = {
+    snapshot: {
+      paramMap: {
+        get: (key: string) => {
+          if (key === 'id') return '1';
+          return null;
+        }
+      }
+    }
+  };
 
-//     fixture = TestBed.createComponent(PoseDetailsComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(async () => {
+    const poseServiceSpy = jasmine.createSpyObj<PosesFetchService>(['getOnePose']);
+    poseServiceSpy.getOnePose.and.callFake((id: number) => {
+      if (id === 1) {
+        return of(mockPose);
+      }
+      return throwError(() => {
+        const error: any = new Error(`This is error number ${HttpStatusCode.NotFound}`);
+        error.timestamp = Date.now();
+        return error;
+      });
+    });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    await TestBed.configureTestingModule({
+      imports: [PoseDetailsComponent],
+      providers: [
+        {
+          provide: PosesFetchService,
+          useValue: poseServiceSpy
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: mockedId
+        },
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PoseDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', fakeAsync(() => {
+    component.ngOnInit();
+    tick();
+    fixture.detectChanges();
+
+    expect(component).toBeTruthy();
+  }));
+
+  it('should fetch yoga pose details', fakeAsync(() => {
+    component.ngOnInit();
+    tick();
+    fixture.detectChanges();
+    // console.log(fixture.debugElement.query(By.css('.pose-name')))
+    let poseName = fixture.debugElement.query(By.css('.pose-name'));
+    expect(fixture.debugElement.query(By.css('.pose-name'))).toBeTruthy();
+    expect(poseName.nativeNode.textContent).toBe('Downward-Facing Dog Pose');
+  }));
+
+  it('should format the name, adding "Pose" if it is not already there', () => {
+    expect(component.formatName('Tree Pose')).toBe('Tree Pose');
+
+    expect(component.formatName('Tree')).toBe('Tree Pose');
+    expect(component.formatName('Mountain')).toBe('Mountain Pose');
+  });
+});
--- src/app/components/pose-details/pose-details.component.ts
+++ src/app/components/pose-details/pose-details.component.ts
@@ -62,23 +62,4 @@ export class PoseDetailsComponent {
       return `${name} Pose`;
     }
   }
-}
- // This method is called when the component is initialized
-  // It retrieves the pose ID from the route parameters and fetches the pose data
-  // If the data is already available, it uses that instead of fetching again
-  // It also handles any errors that may occur during the fetch operation
-  // The fetched data is stored in the 'data' property of the component
-  // async ngOnInit() {
-  //   // Number(this.route.snapshot.paramMap.get('id')) retrieves the 'id' parameter from the URL
-  //   // and converts it to a number. If the parameter is not present, it will be null.
-  //   this.id = Number(this.route.snapshot.paramMap.get('id'));
-  //   if (this.data === null && this.id) {
-  //     // Try is tying to fetch pose data using the PosesFetchService
-  //     // If the fetch is successful, the data will be stored in this.data
-  //     try {
-  //       this.data = await this.posesFetchService.getOnePose(this.id);
-  //     } catch (error) {
-  //       console.error('Error fetching pose:', error);
-  //     }
-  //   }
-  // }; 
+};
