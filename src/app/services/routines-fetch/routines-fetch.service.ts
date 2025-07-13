a7dc25ddf7280cd57cac14b934aa3c8807380d19
Renee Messersmith <morganrmessersmith@gmail.com>
2025-07-13 13:09:48 -0400

    Feat: replaced async fetch requests with http client observables

--- src/app/services/routines-fetch/routines-fetch.service.ts
+++ src/app/services/routines-fetch/routines-fetch.service.ts
@@ -1,45 +1,62 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Pose {
+  pose_id: number;
+  name: string;
+  sanskrit_name: string;
+  image_url: string;
+  description: string;
+  translation_name: string;
+  pose_benefits: string;
+}
+
+export interface RoutineAttributes {
+  name: string;
+  description: string;
+  difficulty: string;
+  routine_poses: Pose[];
+}
+
+export interface RoutineItem {
+  id: string;
+  type: string;
+  attributes: RoutineAttributes;
+}
+
+export interface RoutineResponse {
+  data: RoutineItem[];
+}
+
+export interface RoutineDetails {
+  data: RoutineItem;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoutinesFetchService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
-  async fetchRoutines() {
-    const url = "http://localhost:3000/api/v1/routines";
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      }
-    });
+  // token: string = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE3NTI0Mzg1NjN9.M70A5v6IYLUUJSTmp-KVRpIywbKYvI3QLvaeLem_D48';
+  // headers = new HttpHeaders().set('Authorization', `Bearer ${this.token}`)
+  // httpHeaders: HttpHeaders = new HttpHeaders({
+  //   Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE3NTI0Mzg1NjN9.M70A5v6IYLUUJSTmp-KVRpIywbKYvI3QLvaeLem_D48'
+  // })
 
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    };
+  getRoutines(): Observable<RoutineResponse> {
+    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE3NTI0Mzk3NjZ9.AWmZimkWsr2hyxG8zd-jjRs0W-KosIkgw4ImTqkQGO0';
+    // const httpHeaders = new HttpHeaders({
+    //   'Authorization': `Bearer ${token}`
+    // });
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    const json = await response.json();
-    return json;
+    return this.httpClient.get<RoutineResponse>('http://localhost:3000/api/v1/routines', { headers });
   };
 
-  async fetchRoutineDetails(id: number) {
-    const url = `http://localhost:3000/api/v1/routines/${id}`;
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    };
-
-    const json = await response.json();
-    return json.data.attributes;
-  }
+  getOneRoutine(id: number): Observable<RoutineDetails> {
+    return this.httpClient.get<RoutineDetails>(`http://localhost:3000/api/v1/routines/${id}`);
+  };
 }
