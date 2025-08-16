09c30155b8f283fa460869d523c2ec9118399dec
Renee Messersmith <morganrmessersmith@gmail.com>
2025-08-15 20:33:27 -0400

    Feat: moved state to app file, removed repeat interfaces, added user name to auth state

--- src/store/app.state.ts
+++
@@ -1,9 +0,0 @@
-import { AuthState } from './auth/auth.reducer';
-
-export interface AppState {
-  auth: AuthState;
-}
-// The default state when the app starts -- no token
-export const initialState: AuthState = {
-  token: null
-};
