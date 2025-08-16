2c8b67da5b45d9748bab658de1f1d17e7a47e1e0
Renee Messersmith <morganrmessersmith@gmail.com>
2025-08-15 20:47:21 -0400

    Test: updated home component tests to match updates and include a mock store

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -2,15 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 import { By } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let mockStore: jasmine.SpyObj<Store>;
 
   beforeEach(async () => {
+    mockStore = jasmine.createSpyObj('Store', ['dispatch', 'select']);
+    mockStore.select.and.returnValue(of('Dolly P'));
+
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
-      providers: [provideRouter([])]
+      providers: [
+        provideRouter([]),
+        { provide: Store, useValue: mockStore},
+      ]
     })
     .compileComponents();
 
@@ -23,9 +32,9 @@ describe('HomeComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should display the welcome message', () => {
+  it('should display the welcome message with the user name', () => {
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h2')?.textContent).toContain('Welcome to YogaFlow!');
+    expect(compiled.querySelector('h2')?.textContent).toContain('Welcome Dolly P!');
     expect(compiled.querySelector('.welcome-text')?.textContent)
       .toContain('Namaste');
   });
@@ -39,10 +48,10 @@ describe('HomeComponent', () => {
 
   it('should display navigation buttons', () => {
     const navLinks = fixture.debugElement.queryAll(By.css('.nav-buttons a'));
-    expect(navLinks.length).toBe(3); // three <a> elements
+    expect(navLinks.length).toBe(2); // three <a> elements
     expect(navLinks[0].nativeElement.textContent).toContain('Explore Poses');
     expect(navLinks[1].nativeElement.textContent).toContain('View Routines');
-    expect(navLinks[2].nativeElement.textContent).toContain('Create A New Routine');
+    // expect(navLinks[2].nativeElement.textContent).toContain('Create A New Routine');
   });
 
 });
