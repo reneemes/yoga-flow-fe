a79a17ca50dae99ae390c42d308b93cd5a98f5ce
Renee Messersmith <morganrmessersmith@gmail.com>
2025-08-19 16:41:50 -0400

    Fix: remove token console log

--- src/app/services/routines-fetch/routines-fetch.service.ts
+++ src/app/services/routines-fetch/routines-fetch.service.ts
@@ -25,7 +25,6 @@ export class RoutinesFetchService {
 
   getRoutines(): Observable<Routine> {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${this.tokenSubject.value}`);
-    console.log("HERE <><<>>", this.tokenSubject.value)
 
     return this.httpClient.get<Routine>('https://yoga-flow-7a813c31e5f1.herokuapp.com/api/v1/routines', { headers });
     // return this.httpClient.get<Routine>('http://localhost:3000/api/v1/routines', { headers });
